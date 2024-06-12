18902e8a84187b99c9c9ab5f590ff09317ef8bba
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-06-11 22:23:48 -0400

    Adding Projects

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
-// import path from "node:path";
 import postcss from "./postcss.config.js";
 import { terser } from "rollup-plugin-terser";
 
-//  const isGitHubPages = true;
-//  const folderName = path.basename(process.cwd()) + "/";
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-//  const base = mode === "production" && isGitHubPages ? "/" + folderName : "./";
 const base = "./";
 
 export default defineConfig({
